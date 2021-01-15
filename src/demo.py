d7cd2889a6a27ff742c6bb20aa20bfae38391cdd
nadia-mourao <nadialm_16@hotmail.com>
2021-01-15 16:39:27 +0000

    Update demo.py

--- src/demo.py
+++ src/demo.py
@@ -1,5 +1,13 @@
 import csv
-with open('dataset/overview.csv', 'r') as file:
+
+print("Bounding Box results:")
+with open('dataset/clasification_analises_bbs.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
+
+print("Real Lesion Contours results:")        
+with open('dataset/clasification_analises_bbs.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
         print(row)
